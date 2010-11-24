2a6e3347e74780e9b707f6d75312bc98762ed67b
Islon <rarestorm@hotmail.com>
2010-11-24 00:30:37 -0200

    first draft

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojure-crawl "0.0.1-SNAPSHOT"
   :description "A dungeon crawler in clojure"
   :dependencies [[clojure "1.2.0"]
-		 [clojure-contrib "1.2.0"]])
+		 [clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
