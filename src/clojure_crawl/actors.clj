0c544510713f0e174ecf23a740e32a36a258bb61
Islon <rarestorm@hotmail.com>
2010-11-25 00:45:14 -0200

    change of mind 1

--- src/clojure_crawl/actors.clj
+++ src/clojure_crawl/actors.clj
@@ -1,3 +1,9 @@
 (ns clojure-crawl.actors)
 
-(defrecord Player [name race clazz strength agility health skill skills exp life max-life mana max-mana level equip bag])
+(defrecord Race [name description])
+
+(defrecord Player [name race clazz strength agility health magic skills exp life max-life mana max-mana level equip bag])
+
+(defrecord Enemy [name description clazz strength agility health magic skills life max-life mana max-mana level])
+
+(defprotocol )
